2fb0592c229dedf8501a2c973ad19767aab6240e
zBerika <95876410+zBerika@users.noreply.github.com>
2024-11-23 17:11:00 +0400

    Update auto_qarxana.py

--- auto_qarxana.py
+++ auto_qarxana.py
@@ -1,5 +1,5 @@
 class Qarxana:
-    def __init__(self, name, location, distance_to_Georgia = 700):
+    def __init__(self, name, location):
         self.name = name
         self.location = location
 
@@ -7,27 +7,28 @@ class Qarxana:
         return f"{self.name} - {self.location}"
 
 class Engine:
-    def __init__(self, volume, h_p):
+    def __init__(self, engine_type, volume, h_p):
+        self.engine_type = engine_type
         self.volume = volume
         self.h_p=h_p
     def display_info(self):
-        return f"{self.volume}L, {self.h_p} h.p."
+        return f"{self.engine_type}-{self.volume}L, {self.h_p} h.p."
 
-class Car(Qarxana):
-    def __init__(self, qarxana, color, model, year, engine):
-        super().__init__(qarxana.name, qarxana.location)
+class Car():
+    def __init__(self, qarxana, color, model, year, engine: Engine):
+        self.qarxana  = qarxana
         self.model = model
         self.year = year
-        self.engine = engine
         self.color = color
+        self.engine=engine
 
     def display_info(self):
-        engine_info = self.engine.display_info()
-        return f"{self.year} {self.color} {self.model} with {engine_info}"
+        return f"{self.year} {self.color} {self.model}, Qawxana: {self.qarxana.display_info()}, Dzravi: {self.engine.display_info()}"
 
 class Ford(Car):
-    def __init__(self, qarxana, model, color, year, engine):
+    def __init__(self, qarxana, model, color, year,engine):
         super().__init__(qarxana, model, color, year, engine)
+
 
 class Toyota(Car):
     def __init__(self, qarxana, model, color, year, engine):
