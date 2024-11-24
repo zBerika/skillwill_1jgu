03fafed45f3f3352c61b02cd03270938cf94b37d
zBerika <95876410+zBerika@users.noreply.github.com>
2024-11-24 18:21:41 +0400

    Update zakazi.py

--- zakazi.py
+++ zakazi.py
@@ -1,16 +1,14 @@
-import random  # Импортируем модуль random для генерации случайных значений
+import random
 import time
 import os
 
-# Списки данных
+
 car_models = ["Toyota Camry", "Honda Accord", "Ford Mustang", "Chevrolet Malibu", "Nissan Altima"]
 body_types = ["sedan", "coupe", "hatchback", "SUV", "wagon"]
 colors = ["blue", "black", "red", "white", "gray"]
 engine_type = ["Gas", "Diesel"]
 engine_volume = [1.5, 2.0, 2.5, 3.0]
 engine_hp = [125, 150, 175, 200]
-
-# Список клиентов
 clients_names = [
     {"id": 1, "name": "Giorgi"}, {"id": 2, "name": "Nino"}, {"id": 3, "name": "Levan"},
     {"id": 4, "name": "Tamara"}, {"id": 5, "name": "Irakli"}, {"id": 6, "name": "Mariam"},
@@ -19,14 +17,14 @@ clients_names = [
     {"id": 13, "name": "Vano"}, {"id": 14, "name": "Teimuraz"}, {"id": 15, "name": "Marina"}
 ]
 
-# Класс для клиента
+
 class Client:
     def __init__(self, id, name):
-        self.__id = id  # Инициализация идентификатора клиента
-        self.__name = name  # Инициализация имени клиента
+        self.__id = id
+        self.__name = name
 
     def get_id(self):
-        return self.__id  # Метод для получения идентификатора клиента
+        return self.__id
 
     def get_name(self):
         return self.__name
@@ -46,18 +44,17 @@ def print_purchase_info(client, car_model, car_color, car_body_type, car_engine_
 
 if __name__ == "__main__":
     with open('sia.txt', 'w', encoding='utf-8') as file:
-        # Генерируем информацию о покупке для 10 клиентов
         random_clients = random.sample(clients_names, 10)  # Случайный выбор 10 клиентов
         for client_data in random_clients:
-            client = Client.random_client(client_data)  # Генерация клиента
-            car_model = random.choice(car_models)  # Выбор случайной модели автомобиля
-            car_color = random.choice(colors)  # Выбор случайного цвета
-            car_body_type = random.choice(body_types)  # Выбор случайного типа кузова
+            client = Client.random_client(client_data)
+            car_model = random.choice(car_models)
+            car_color = random.choice(colors)а
+            car_body_type = random.choice(body_types)
             car_engine_tupe = random.choice(engine_type)
-            car_engine_volume = random.choice(engine_volume)  # Выбор случайного магазина
+            car_engine_volume = random.choice(engine_volume)
             car_engine_hp = random.choice(engine_hp)
 
-            # Вывод информации о покупке
+
             print_purchase_info(client, car_model, car_color, car_body_type, car_engine_tupe, car_engine_volume, car_engine_hp)
 
 os.system('python auto_qarxana.py')
